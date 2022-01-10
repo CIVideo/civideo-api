d1cf408952707bbc96659b92c518f4a17e7ba6cc
jaeho-jang <lemphis@gmail.com>
2022-01-10 19:40:04 +0900

    Initial commit

---
+++ civideo/src/main/java/com/jsss/civideo/CivideoApplication.java
@@ -0,0 +1,13 @@
+package com.jsss.civideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivideoApplication.class, args);
+	}
+
+}
---
+++ civideo/src/test/java/com/jsss/civideo/CivideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsss.civideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
