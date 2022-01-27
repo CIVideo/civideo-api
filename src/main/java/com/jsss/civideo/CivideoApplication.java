0d3370f0b42014f74326107c701c848ccd62f6c0
jaeho-jang <lemphis@gmail.com>
2022-01-27 16:49:05 +0900

    Add JPA auditing

--- src/main/java/com/jsss/civideo/CivideoApplication.java
+++ src/main/java/com/jsss/civideo/CivideoApplication.java
@@ -2,12 +2,14 @@ package com.jsss.civideo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CivideoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CivideoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CivideoApplication.class, args);
+    }
 
 }
