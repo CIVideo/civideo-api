cea428aaf760d328aedc0a24803211eaa8578d39
jaeho-jang <jaehojang@nate.com>
2022-02-02 16:55:13 +0900

    Disable test code

--- src/test/java/com/jsss/civideo/CivideoApplicationTests.java
+++ src/test/java/com/jsss/civideo/CivideoApplicationTests.java
@@ -1,11 +1,11 @@
 package com.jsss.civideo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("dev")
-@SpringBootTest
+// @SpringBootTest
 class CivideoApplicationTests {
 
 	@Test
