211e55db3170d0707d70533dd4238ad5056c0ac3
jaeho-jang <lemphis@gmail.com>
2022-02-14 16:19:15 +0900

    Update OAuth2 handlers
    * Add CookieUtil
    * SuccessHandler
    * FailureHandler
    * Security Config
    * Request Repository

---
+++ src/main/java/com/jsss/civideo/domain/auth/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,34 @@
+package com.jsss.civideo.domain.auth;
+
+import com.jsss.civideo.global.util.CookieUtil;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.jsss.civideo.domain.auth.OAuth2AuthorizationRequestRepository.*;
+
+@Component
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String redirectUriAfterLogin = CookieUtil.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse(MAIN_URL);
+        redirectUriAfterLogin = UriComponentsBuilder.fromUriString(redirectUriAfterLogin)
+                .queryParam("error", exception.getLocalizedMessage())
+                .build()
+                .toUriString();
+        CookieUtil.removeCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+        CookieUtil.removeCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
+        getRedirectStrategy().sendRedirect(request, response, redirectUriAfterLogin);
+    }
+
+}
---
+++ src/main/java/com/jsss/civideo/global/util/CookieUtil.java
@@ -0,0 +1,72 @@
+package com.jsss.civideo.global.util;
+
+import org.springframework.util.SerializationUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
+import java.util.Base64;
+import java.util.Optional;
+
+public final class CookieUtil {
+
+    private CookieUtil() {
+    }
+
+    public static Optional<Cookie> getCookie(HttpServletRequest req, String cookieName) {
+        Cookie[] cookies = req.getCookies();
+        if (isEmpty(cookies) || cookieName.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return find(cookies, cookieName);
+    }
+
+    public static void addCookie(HttpServletResponse res, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        res.addCookie(cookie);
+    }
+
+    public static void removeCookie(HttpServletRequest req, HttpServletResponse res, @NotNull String cookieName) {
+        Cookie[] cookies = req.getCookies();
+        if (isEmpty(cookies)) {
+            return;
+        }
+        findAndRemove(res, cookies, cookieName);
+    }
+
+    public static String serialize(Object object) {
+        return Base64.getUrlEncoder().encodeToString(SerializationUtils.serialize(object));
+    }
+
+    public static <T> T deserialize(Cookie cookie, Class<T> clazz) {
+        return clazz.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(cookie.getValue())));
+    }
+
+    private static boolean isEmpty(Cookie[] cookies) {
+        return cookies == null || cookies.length == 0;
+    }
+
+    private static Optional<Cookie> find(Cookie[] cookies, String cookieName) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(cookieName)) {
+                return Optional.of(cookie);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private static void findAndRemove(HttpServletResponse res, Cookie[] cookies, String cookieName) {
+        find(cookies, cookieName).ifPresent((cookie) -> {
+            cookie.setValue("");
+            cookie.setPath("/");
+            cookie.setMaxAge(0);
+            res.addCookie(cookie);
+        });
+    }
+
+}
