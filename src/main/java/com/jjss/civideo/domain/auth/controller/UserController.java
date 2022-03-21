391e9c1e2c19b866a21f5dd2cc956fc16d581b9d
jaeho-jang <lemphis@gmail.com>
2022-03-21 10:04:44 +0900

    Remove Swagger

--- src/main/java/com/jjss/civideo/domain/auth/controller/UserController.java
+++ src/main/java/com/jjss/civideo/domain/auth/controller/UserController.java
@@ -3,11 +3,7 @@ package com.jjss.civideo.domain.auth.controller;
 import com.jjss.civideo.domain.auth.dto.TokenRequestDto;
 import com.jjss.civideo.domain.auth.dto.TokenResponseDto;
 import com.jjss.civideo.domain.auth.service.UserService;
-import com.jjss.civideo.global.exception.dto.BadRequestResponseDto;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import com.jjss.civideo.global.exception.BadRequestResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,16 +18,11 @@ public class UserController {
 
     private final UserService userService;
 
-    @Operation(summary = "Access token 발급", description = "provider에서 제공하는 access token을 검증하여 인증 토큰(JWT)을 발급합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = TokenResponseDto.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = BadRequestResponseDto.class)))
-    })
-    @io.swagger.annotations.ApiResponses({
-            @io.swagger.annotations.ApiResponse(code = 200, message = "OK", response = TokenResponseDto.class),
-            @io.swagger.annotations.ApiResponse(code = 400, message = "Bad Request", response = BadRequestResponseDto.class)
-    })
     @GetMapping("/auth/token")
-    public ResponseEntity<?> sendToken(@Valid TokenRequestDto tokenRequestDto) {
+    public ResponseEntity<?> sendToken(@Valid TokenRequestDto tokenRequestDto, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(BadRequestResponseDto.of(errors));
+        }
         String accessToken = userService.createAccessToken(tokenRequestDto);
         if (accessToken == null) {
             return ResponseEntity.badRequest().body(Map.of("message", "invalid token"));
--- src/main/java/com/jjss/civideo/domain/auth/dto/TokenResponseDto.java
+++ src/main/java/com/jjss/civideo/domain/auth/dto/TokenResponseDto.java
@@ -1,15 +1,10 @@
 package com.jjss.civideo.domain.auth.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
-import lombok.Getter;
 
-@Schema(description = "Token Response DTO")
-@Getter
 @Builder
 public class TokenResponseDto {
 
-    @Schema(description = "JWT Token",  example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.hjQ4oomXpzjuXTSj5Th7CsDbBvLbO-WlhgWDdVayRNU")
     private String accessToken;
 
 }
