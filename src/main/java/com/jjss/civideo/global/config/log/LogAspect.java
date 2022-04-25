b2214a27c2843d4c741510907c3e4170cfcb73b1
lemphis <lemphis@gmail.com>
2022-04-25 13:56:24 +0900

    Apply AOP to GlobalExceptionHandler

---
+++ src/main/java/com/jjss/civideo/global/config/log/LogAspect.java
@@ -0,0 +1,20 @@
+package com.jjss.civideo.global.config.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LogAspect {
+
+    @Before("execution(* com.jjss.civideo.global.exception.config.GlobalExceptionHandler.*(*))")
+    public void before(JoinPoint joinPoint) {
+        Exception exception = (Exception) joinPoint.getArgs()[0];
+        log.error(exception.getClass().getSimpleName(), exception);
+    }
+
+}
